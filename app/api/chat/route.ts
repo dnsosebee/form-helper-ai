586785f6fa2109f8aff9ace4b617a228b2ee912b
Daniel Sosebee <dnsosebee@gmail.com>
2023-10-25 14:50:19 -0700

    init example repo

---
+++ app/api/chat/route.ts
@@ -0,0 +1,143 @@
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import OpenAI from "openai";
+import type { ChatCompletionCreateParams } from "openai/resources/chat";
+// Create an OpenAI API client (that's edge friendly!)
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY || "",
+});
+// import {z} from "zod";
+// import {zodToJsonSchema} from "zod-to-json-schema";
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = "edge";
+
+const intent_classification_functions: ChatCompletionCreateParams.Function[] = [
+  {
+    name: "classify_intent",
+    description:
+      "Determine if user wants a list of recipes, or if they want general information about RecipeBot.",
+    parameters: {
+      type: "object",
+      properties: {
+        intent: {
+          type: "string",
+          enum: ["recipes", "general_info"],
+          description: "The user intent classification.",
+        },
+      },
+      required: ["intent"],
+    },
+  },
+];
+
+const recipe_response_functions: ChatCompletionCreateParams.Function[] = [
+  {
+    name: "create_recipes_response",
+    description: "Create a response to the user based on the recipes they want.",
+    parameters: {
+      type: "object",
+      properties: {
+        introduction: {
+          type: "string",
+          description:
+            "The introduction to the response. Can be something like 'Here are some recipes for you:', but more creative to fit the user's question.",
+        },
+        recipes: {
+          type: "array",
+          items: {
+            type: "object",
+            properties: {
+              context: {
+                type: "string",
+                description:
+                  'Context to be provided above the link to this particular recipe. For example, "A delicious vegan soup".',
+              },
+              link: {
+                type: "string",
+                description: "A link to the recipe.",
+              },
+            },
+            required: ["context", "link"],
+          },
+        },
+        followUp1: {
+          type: "string",
+          description:
+            "A follow up question that the user may want to ask you. For example, 'Would you like to see more recipes?'",
+        },
+        followUp2: {
+          type: "string",
+          description:
+            "A follow up question that the user may want to ask you. For example, 'Would you like to see more recipes?'. Must differ from followUp1.",
+        },
+      },
+      required: ["introduction", "recipes", "followUp1", "followUp2"],
+    },
+  },
+];
+
+export async function POST(req: Request) {
+  const json = await req.json();
+  console.log("json", json);
+  const { messages } = json;
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo-0613",
+    stream: true,
+    messages,
+    functions: intent_classification_functions,
+    function_call: {
+      name: "classify_intent",
+    },
+  });
+
+  // const data = new experimental_StreamData();
+  const stream = OpenAIStream(response, {
+    // experimental_onFunctionCall: async ({ name, arguments: args }, createFunctionCallMessages) => {
+    //   if (args.intent === "recipes") {
+    //     const recipes = [
+    //       {
+    //         title: "Sweet potato and coconut soup",
+    //         link: "https://www.bbcgoodfood.com/recipes/speedy-sweet-potato-soup-coconut",
+    //       },
+    //       {
+    //         title: "Vegan leek & potato soup",
+    //         link: "https://www.bbcgoodfood.com/recipes/vegan-leek-potato-soup",
+    //       },
+    //     ];
+
+    //     // data.append(recipes);
+
+    //     const newMessages = createFunctionCallMessages(recipes);
+    //     return openai.chat.completions.create({
+    //       messages: [...messages, ...newMessages],
+    //       stream: true,
+    //       model: "gpt-3.5-turbo-0613",
+    //       functions: recipe_response_functions,
+    //       function_call: {
+    //         name: "create_recipes_response",
+    //       },
+    //     });
+    //   } else if (args.intent === "general_info") {
+    //     return openai.chat.completions.create({
+    //       messages: [...messages],
+    //       stream: true,
+    //       model: "gpt-3.5-turbo-0613",
+    //     });
+    //   }
+    // },
+    onCompletion(completion) {
+      console.log("completion", completion);
+    },
+    onFinal(completion) {
+      // data.close();
+    },
+    // experimental_streamData: true,
+  });
+
+  // data.append({
+  //   text: "Hello, how are you?",
+  // });
+
+  return new StreamingTextResponse(stream, {});
+}
---
+++ app/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { FunctionCallHandler } from "ai";
+import { Message, useChat } from "ai/react";
+
+export default function Chat() {
+  const functionCallHandler: FunctionCallHandler = async (chatMessages, functionCall) => {
+    console.log("\n\n\n");
+    console.log("functionCall", functionCall);
+    console.log("chatMessages", chatMessages);
+    return;
+  };
+
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    api: "/api/chat",
+    experimental_onFunctionCall: functionCallHandler,
+  });
+
+  // Generate a map of message role to text color
+  const roleToColorMap: Record<Message["role"], string> = {
+    system: "red",
+    user: "black",
+    function: "blue",
+    assistant: "green",
+  };
+
+  console.log("\n\n\n");
+  // console.log("data", data);
+  console.log("messages", messages);
+
+  return (
+    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+      {messages.length > 0
+        ? messages.map((m: Message) => (
+            <div
+              key={m.id}
+              className="whitespace-pre-wrap"
+              style={{ color: roleToColorMap[m.role] }}
+            >
+              <strong>{`${m.role}: `}</strong>
+              {m.content || JSON.stringify(m.function_call)}
+              <br />
+              <br />
+            </div>
+          ))
+        : null}
+      <div id="chart-goes-here"></div>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+          value={input}
+          placeholder="Say something..."
+          onChange={handleInputChange}
+        />
+      </form>
+    </div>
+  );
+}
